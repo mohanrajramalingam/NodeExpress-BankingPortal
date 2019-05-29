09d98e78155ae7511b2ddc3e8d245d473b0df534
Mohanraj Ramalingam <mohanraj.ramalingam@wolterskluwer.com>
2019-05-29 15:57:42 -0500

    Completed the module

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const app = express();
 
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,40 +29,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 
-app.get('/savings', (req, res) => {
-    res.render('account', { account: accounts.savings });
-});
+// app.get('/savings', (req, res) => {
+//     res.render('account', { account: accounts.savings });
+// });
 
-app.get('/checking', (req, res) => {
-    res.render('account', { account: accounts.checking });
-});
+// app.get('/checking', (req, res) => {
+//     res.render('account', { account: accounts.checking });
+// });
 
-app.get('/credit', (req, res) => {
-    res.render('account', { account: accounts.credit });
-});
+// app.get('/credit', (req, res) => {
+//     res.render('account', { account: accounts.credit });
+// });
 
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    // const accountsJSON = JSON.stringify(accounts, null, 4);
-    // fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    writeJSON();
-    res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    // const accountsJSON = JSON.stringify(accounts, null, 4);
-    // fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
-});
 
 app.get('/profile', (req, res) => { res.render('profile', { user: users[0] }); });
 
