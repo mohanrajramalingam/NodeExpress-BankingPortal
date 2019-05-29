a1357595a88671e45ea5fe17a1d37425f0594db7
Mohanraj Ramalingam <mohanraj.ramalingam@wolterskluwer.com>
2019-05-28 23:30:31 -0500

    Minor changes for transfer and make payment

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.post('/transfer', (req, res) => {
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    res.render('transfer', { message: 'Transfer Completed' });
+    res.render('transfer', { message: 'Transfer Completed...' });
 });
 
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
@@ -55,7 +55,7 @@ app.post('/payment', (req, res) => {
     accounts.credit.available += parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+    res.render('payment', { message: 'Payment Successful !!!', account: accounts.credit });
 });
 
 app.get('/profile', (req, res) => {
