341bc43a03f454b5acbcf74991e97b57b237eb3b
Mohanraj Ramalingam <mohanraj.ramalingam@wolterskluwer.com>
2019-05-29 16:03:19 -0500

    Completed the module

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const express = require('express');
 
-const app = express();
-
 const { accounts, users, writeJSON } = require('./data');
+
 const accountRoutes = require('./routes/accounts');
 const servicesRoutes = require('./routes/services');
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -29,6 +30,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
+app.get('/profile', (req, res) => { res.render('profile', { user: users[0] }); });
+
 // app.get('/savings', (req, res) => {
 //     res.render('account', { account: accounts.savings });
 // });
@@ -42,6 +48,6 @@ app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accou
 // });
 
 
-app.get('/profile', (req, res) => { res.render('profile', { user: users[0] }); });
+
 
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
