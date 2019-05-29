3681fa1c58f9a95a90afb956242d5918d9b2208c
Mohanraj Ramalingam <mohanraj.ramalingam@wolterskluwer.com>
2019-05-28 23:32:45 -0500

    app.js

--- src/app.js
+++ src/app.js
@@ -58,10 +58,6 @@ app.post('/payment', (req, res) => {
     res.render('payment', { message: 'Payment Successful !!!', account: accounts.credit });
 });
 
-app.get('/profile', (req, res) => {
-    res.render('profile', { user: users[0] });
-});
+app.get('/profile', (req, res) => { res.render('profile', { user: users[0] }); });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
-
-
